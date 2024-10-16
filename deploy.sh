ea69c794836f9698b289d0e513dfc909957cf447
Amodh Jaiswal <60597059+amodhjaiswal@users.noreply.github.com>
2024-10-16 18:38:58 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+# Print the current working directory
+echo "Current Directory:"
 pwd
+
+# Navigate to the Node.js application directory
+cd /home/ubuntu/node || { echo "Directory not found! Exiting."; exit 1; }
+
+# Start the Node.js application
+echo "Starting the application..."
+npm start
